1761d419bb454efabd9417e418ebf75b6e4963b7
Esa Kataja <ekataja@gmail.com>
2021-03-30 22:46:24 +0300

    ADDED: Basic funtionality

--- ccprowrap.py
+++ ccprowrap.py
@@ -1,24 +1,37 @@
 from subprocess import run
 from argparse import ArgumentParser
 
+#TODO check fanlist
+allFans = [1,2,3,4,5,6]
+
 def getCPUTemp():
     #TODO Add logic
     return 0
 
 def changeFanSpeed(fans, speed):
+    
+    if len(fans) == 0:
+        return False
+    print(f'Trying to set the speed of fans {str(fans)}')
     for fan in fans:
-        #FIXME Check if fans is a integer array and speed is integer between 0 and 100
-        if not 0 <= speed <= 100:
+        if not 0 <= int(speed) <= 100:
             #TODO Add logger
             print("ERROR: invalid speed. Must be between 1 - 100")
-        cmd = f'liquidctl set fan{fan} speed {speed}'
-    #TODO Add logic
-    pass
+            return False
+        cmd = f'liquidctl set fan{str(fan)} speed {speed}'.split(" ")
+        try:
+            run(cmd)
+        except FileNotFoundError:
+            print("liquidctl not found. Is it installed?")
+            exit(0)
 
 def listFans():
+    #TODO Add logic
     return None
 
 argparser = ArgumentParser(description="Spin that fan!\nHelper for liquidctl")
 argparser.add_argument("fanspeed",help="Set fan speed as percent (0-100)", type=int)
 
 args = argparser.parse_args()
+
+changeFanSpeed(allFans, args.fanspeed)
